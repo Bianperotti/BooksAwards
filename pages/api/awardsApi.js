f3f03b2faa1aeccc82069e8f072b3530acdf556a
bianca <bianca.perotti@hotmail.com>
2022-06-03 16:48:24 +0200

    Add getAward api

--- pages/api/awardsApi.js
+++ pages/api/awardsApi.js
@@ -1,9 +1,19 @@
 export async function getAwards() {
   const awardsReq = await fetch(
-    `https://stingray-app-ozczk.ondigitalocean.app/items/awards?fields=id,name,colours,awarded_books.id,awarded_books.votes,awarded_books.book.title,awarded_books.book.id,awarded_books.book.cover`
+    `https://stingray-app-ozczk.ondigitalocean.app/items/awards?fields=id,name,slug,colours,awarded_books.id,awarded_books.votes,awarded_books.book.title,awarded_books.book.id,awarded_books.book.cover`
   )
 
   const { data: awards } = await awardsReq.json()
 
   return awards
+}
+
+export async function getAward(slug) {
+  const awardReq = await fetch(
+    `https://stingray-app-ozczk.ondigitalocean.app/items/awards?fields=id,name,slug,colours,awarded_books.id,awarded_books.votes,awarded_books.book.title,awarded_books.book.id,awarded_books.book.cover&filter[slug][_eq]=${slug}`
+  )
+
+  const { data: award } = await awardReq.json()
+
+  return award.length === 0 ? false : award[0]
 }
