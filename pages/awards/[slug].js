780fff5e93b026adcc079e76b73b65e39381635d
bianca <bianca.perotti@hotmail.com>
2022-06-03 16:48:09 +0200

    Add voting oage

---
+++ pages/awards/[slug].js
@@ -0,0 +1,39 @@
+import { getAward } from '../api/awardsApi'
+import BookCard from '../../components/BookCard'
+
+export async function getServerSideProps(context) {
+  const slug = context.params.slug
+  const award = await getAward(slug)
+
+  if (!award) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/',
+      },
+      props: {},
+    }
+  }
+
+  return { props: { award } }
+}
+
+const FirstAward = ({ award }) => {
+
+  return (
+    <div className="flex flex-col items-center justify-center py-2">
+      <main className="container flex flex-col items-center justify-center flex-1 w-full max-w-5xl px-6 font-semibold lg:px-0 ">
+        <h1 className="text-5xl font-bold">{award.name}</h1>
+        <input
+          className="block w-full py-2 pr-3 bg-white border rounded-md shadow-sm border-slate-300 pl-9 placeholder:italic placeholder:text-slate-400 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
+          placeholder="Vote another book..."
+        ></input>
+        <div className="grid grid-cols-2 mt-16 gap-x-8 gap-y-16 sm:grid-cols-3 lg:grid-cols-5">
+          <BookCard award={award} />
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default FirstAward
