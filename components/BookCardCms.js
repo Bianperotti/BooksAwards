ed5eb7ebe4b2e2a259e233dd9bc72eeb738a7f27
bianca <bianca.perotti@hotmail.com>
2022-06-08 19:07:02 +0200

    Add vote to button

--- components/BookCardCms.js
+++ components/BookCardCms.js
@@ -1,4 +1,7 @@
+import { updateVotes } from '../pages/api/awardsApi'
+
 const BookCardCms = ({ award }) => {
+
   const votedBooks = award.awarded_books
 
   return (
@@ -9,7 +12,10 @@ const BookCardCms = ({ award }) => {
             className="relative h-56 rounded-lg -top-2 w-36"
             src={`https://stingray-app-ozczk.ondigitalocean.app/assets/${book.book.cover}`}
           ></img>
-          <button className="px-4 py-2 text-xl text-white rounded w-36 bg-orange-500/75 hover:bg-orange-700/75 ">
+          <button
+            onClick={() => updateVotes(book.id)}
+            className="px-4 py-2 text-xl text-white rounded w-36 bg-orange-500/75 hover:bg-orange-700/75 "
+          >
             vote
           </button>
         </div>
