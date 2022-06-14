4d5ead03a1249486750a983e07c55a6d9ef6f390
bianca <bianca.perotti@hotmail.com>
2022-06-14 10:37:18 +0200

    Add href to logo navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,11 @@
 export default function Navbar() {
   return (
     <header className="p-6">
-      <h2 className="font-sans text-lg font-semibold ">
-        Random books awards
-      </h2>
+      <a href={`/`}>
+        <h2 className="font-sans text-lg font-semibold ">
+          Random books awards
+        </h2>
+      </a>
     </header>
   )
 }
