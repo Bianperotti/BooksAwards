03da03c7d30f5e85f7a989aa5ad6f2670f7f690e
bianca <bianca.perotti@hotmail.com>
2022-06-15 19:40:17 +0200

    Improve UI navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,9 +2,8 @@ export default function Navbar() {
   return (
     <header className="p-6">
       <a href={`/`}>
-        <h2 className="font-sans text-lg font-semibold ">
-          Random books awards
-        </h2>
+        <h2 className="font-sans text-lg font-semibold md:hidden">Rba</h2>
+        <h2 className="hidden font-sans text-lg font-semibold md:block">Random books awards</h2>
       </a>
     </header>
   )
