a48648ce4fc1ec068d7e6e383c928a8490c776cb
bianca <bianca.perotti@hotmail.com>
2022-06-15 17:36:15 +0200

    Footer improve

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,17 @@
-import Image from 'next/image'
-
 export default function Footer() {
   return (
     <footer className="flex items-center justify-center w-full h-24 border-t">
-      <a
-        className="flex items-center justify-center gap-2"
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{' '}
-        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-      </a>
+      <div>
+        <span>made with ❤️ by </span>
+        <a href="https://github.com/Bianperotti" className="underline">
+          BianPerotti
+        </a>
+        <span>  -  idea by </span>
+        <a href="https://www.tiktok.com/@jaydenkouli" className="underline">
+          Jaydenkouli
+        </a>
+      </div>
+    
     </footer>
   )
 }
