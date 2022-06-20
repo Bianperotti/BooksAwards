fdf75f06dff35e8677327f0e3636cca019ecf085
bianca <bianca.perotti@hotmail.com>
2022-06-20 17:16:14 +0200

    Update padding navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,7 @@ import Share from '../components/Share'
 
 export default function Navbar() {
   return (
-    <header className="flex items-center justify-between px-4 py-6 sm:px-6 lg:px-20 ">
+    <header className="flex items-center justify-between px-4 py-6 pb-10 sm:px-6 lg:px-20 ">
       <a href={`/`} className="h-fit">
         <h2 className="font-sans text-lg font-semibold md:hidden ">Rba</h2>
         <h2 className="hidden font-sans text-lg font-semibold md:block ">
