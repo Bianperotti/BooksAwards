c1364ae2ddaf7cb9f4e7ba30f68c61306f84d9f8
bianca <bianca.perotti@hotmail.com>
2022-06-20 17:09:35 +0200

    Add meta image twitter

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,7 @@ const Home = ({ awards, totalVotes }) => {
           key="title"
         />
         <meta property="og:image" content="/meta_imagen.png" />
+        <meta property="twitter:image" content="/meta_imagen.png" />
       </Head>
       <div className="flex flex-col items-center justify-center py-2">
         <main className="container flex flex-col items-center justify-center flex-1 w-full max-w-5xl px-6 font-semibold lg:px-0 ">
