5d2eb57c9a82ae910afb37fb7d807005f37e8b85
bianca <bianca.perotti@hotmail.com>
2022-08-24 16:59:40 +0200

    Update a to Link

--- components/AwardCard.js
+++ components/AwardCard.js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 const colors = {
   green: 'from-green-500/50 to-emerald-700/50',
   cyan: 'from-cyan-400/50 to-cyan-600/50',
@@ -5,7 +7,7 @@ const colors = {
   red: 'from-red-500/50 to-orange-700/50',
   green2: 'from-green-400/50 to-emerald-600/50',
   pink: 'from-pink-400/50 to-pink-600/50',
-  yellow: 'from-amber-300/50 to-amber-600/50'
+  yellow: 'from-amber-300/50 to-amber-600/50',
 }
 
 const AwardCard = ({ award }) => {
@@ -15,23 +17,23 @@ const AwardCard = ({ award }) => {
   winnerBook.length = 1
 
   return (
-    <a
-      key={award.id}
-      href={`/awards/${award.slug}`}
-      className={`flex h-48 rounded-xl border bg-gradient-to-r p-4 text-left ${colorClasses}`}
-    >
-      <div className="relative">
-        <img
-          className="-top-14 relative h-[12.3rem] w-36 rounded-lg"
-          sizes="50vw"
-          alt="Winner book cover"
-          src={`https://stingray-app-ozczk.ondigitalocean.app/assets/${winnerBook[0].book.cover}`}
-        ></img>
-      </div>
-      <div className="self-center w-3/5 p-4 pr-0">
-        <h2 className="text-xl text-white "> {award.name} </h2>
-      </div>
-    </a>
+    <Link href={`/awards/${award.slug}`}>
+      <a
+        className={`flex h-48 rounded-xl border bg-gradient-to-r p-4 text-left ${colorClasses}`}
+      >
+        <div className="relative">
+          <img
+            className="relative -top-14 h-[12.3rem] w-36 rounded-lg"
+            sizes="50vw"
+            alt="Winner book cover"
+            src={`https://stingray-app-ozczk.ondigitalocean.app/assets/${winnerBook[0].book.cover}`}
+          ></img>
+        </div>
+        <div className="self-center w-3/5 p-4 pr-0">
+          <h2 className="text-xl text-white "> {award.name} </h2>
+        </div>
+      </a>
+    </Link>
   )
 }
 
